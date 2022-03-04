1dd0a02f26a7e0b6b278bf1a0e5c351645d2b54e
Jheave <jimenezjheave123@gmail.com>
2022-03-05 04:02:11 +0800

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,8 +20,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   solidity: '0.8.1',
   networks: {
     rinkeby: {
-      url: env.STAGING_ALCHEMY_KEY,
-      accounts: [env.PRIVATE_KEY],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
