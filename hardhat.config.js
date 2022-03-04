52c45a9c2c1218019a5b0544cf44e638d69c694a
Jheave <jimenezjheave123@gmail.com>
2022-03-05 03:53:35 +0800

    modified

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: env.STAGING_ALCHEMY_KEY,
+      accounts: [env.PRIVATE_KEY],
+    },
+  },
 };
